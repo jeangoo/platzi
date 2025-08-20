0e0fbd4a79a2fc1aca7f8f27d1038ced6ded2af0
Jean <jeaancast03@gmail.com>
2025-08-19 21:18:23 -0400

    Add initial HTML structure and JavaScript functionality for alert on button click

---
+++ JS/javascript-01/app.js
@@ -0,0 +1,27 @@
+function showAlert() {
+  window.addEventListener('click', (
+    alert("hola")
+  ))
+}
+
+const parent = document.getElementById("menu");
+console.log(parent);
+
+if (parent) {
+  const children = parent.children;
+  console.log('There is a children element', children);
+
+} else {
+  console.log('There is no children element')
+}   
+
+const firstChild = parent.firstElementChild;
+
+console.log(firstChild);
+
+const lastChild = parent.lastElementChild;
+console.log(lastChild);
+
+const nextSibling = firstChild.nextElementSibling;
+console.log(nextSibling);
+
