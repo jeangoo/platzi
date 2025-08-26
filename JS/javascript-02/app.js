72dca67a84298762f33c23de98002ba27a5e7f5b
Jean <jeaancast03@gmail.com>
2025-08-26 11:06:15 -0400

    new file, new changes

--- JS/javascript-02/app.js
+++ JS/javascript-02/app.js
@@ -1,45 +1,48 @@
-const list = document.querySelector("ul")
-const listItems = list.getElementsByTagName("li")
-const addBtn = document.getElementById("addbtn")
+const list = document.querySelector("ul");
+const listItems = list.getElementsByTagName("li");
+const addBtn = document.getElementById("addbtn");
 // const delBtn = document.querySelector(".deleteBtn")
-const taskInput = document.querySelector("input")
-const toggleBtn = document.querySelector(".toggle")
+const taskInput = document.querySelector("input");
+const toggleBtn = document.querySelector(".toggle");
+const checkbox = document.querySelector(".checkbox");
 
-
-addBtn.addEventListener('click', () => {
-    let task = taskInput.value
-    if (task !== null && task.trim() !== "") {
-        list.innerHTML += `<li>${task}  <button class="editBtn">Edit</button> <button class="deleteBtn">Delete</button> </li>`
-        taskInput.value = "";
-    } else {
-        alert("Error trying to add the task.")
-    }
-})
+addBtn.addEventListener("click", () => {
+  let task = taskInput.value;
+  if (task !== null && task.trim() !== "") {
+    list.innerHTML += `<li>${task}  <button class="editBtn">Edit</button>  <button class="deleteBtn">Delete</button> </li>`;
+    taskInput.value = "";
+  } else {
+    alert("Error trying to add the task.");
+  }
+});
 
 list.addEventListener("click", (event) => {
-    
-    if (event.target.classList.contains('deleteBtn')) {
-            event.target.parentElement.remove()
-    }
+  if (event.target.classList.contains("deleteBtn")) {
+    event.target.parentElement.remove();
+  }
 
-    if (event.target.classList.contains('editBtn')) {
-        let li = event.target.parentElement;
-        let currentTask = li.firstChild.textContent.trim()
-        let editedTask = prompt(`Edit the task ${currentTask}`) 
-        if (editedTask !== null && editedTask.trim() !== "") {
-            li.innerHTML = `${editedTask}  <button class="editBtn">Edit</button> <button class="deleteBtn">Delete</button>`
-        } else {
-            alert("Error trying to edit the task.")
-        }
+  if (event.target.classList.contains("editBtn")) {
+    let li = event.target.parentElement;
+    let currentTask = li.firstChild.textContent.trim();
+    let editedTask = prompt(`Edit the task ${currentTask}`);
+    if (editedTask !== null && editedTask.trim() !== "") {
+      li.innerHTML = `${editedTask}  <button class="editBtn">Edit</button> <button class="deleteBtn">Delete</button>`;
+    } else {
+      alert("Error trying to edit the task.");
     }
-})
+  }
+});
 
 taskInput.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-        addBtn.click()
-    }
-})
+  if (event.key === "Enter") {
+    addBtn.click();
+  }
+});
 
-toggleBtn.addEventListener('click', () => {
-    document.body.classList.toggle("body-dark")
-})
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("body-dark");
+});
+
+checkbox.addEventListener("click", () => {
+  checkbox.classList.toggle("checked");
+});
