78883dc01a1816cf0d0d1ce009acf6328033373b
Jean <jeaancast03@gmail.com>
2025-09-12 13:32:42 -0400

    changes

---
+++ bundlers/parcel/src/app.js
@@ -0,0 +1,35 @@
+import greetParcel from "./index";
+import { farewellParcel } from "./index";
+import { returnDelayParcel } from "./index";
+import { characterData } from "./index";
+import { vowelsCounter } from "./index";
+
+const $app = document.getElementById("app");
+const $list = document.querySelector("ul");
+
+$app.innerHTML = greetParcel();
+$app.innerHTML += farewellParcel();
+returnDelayParcel();
+
+const {
+  name,
+  image,
+  age,
+  gang,
+  money,
+  friends,
+  atributtes: { health, stamina, deadeye },
+} = characterData;
+console.log(name, image, age, gang, money, health, stamina, deadeye);
+
+friends.forEach((friend) => {
+  $list.innerHTML += `<li>${friend}</li>`;
+});
+
+const text =
+  vowelsCounter(`Parcel's JavaScript, CSS, HTML, and SVG compilers are written in Rust for maximum performance. These are 10-100x faster than other JavaScript-based tools!
+
+Parcel's JavaScript compiler is built on SWC, which handles transpiling JavaScript, JSX, and TypeScript. On top of SWC, Parcel implements dependency collection, bundling, scope hoisting, tree shaking, Node emulation, hot reloading, and more.
+
+Parcel's CSS transformer and minifier is built in Rust on top of the browser-grade CSS parser used in Firefox. It's over 100x faster than other JavaScript-based transformers and minifiers.`);
+console.log(text);
--- bundlers/parcel/src/index.js
+++ bundlers/parcel/src/index.js
@@ -1,15 +1,92 @@
 const $app = document.getElementById("app");
-const API_URL = "";
+const $button = document.querySelector("button");
 
-const main = async () => {
-  const response = await fetch(API_URL);
-  const data = await response.json();
-  const output = data?.map(() => {
-    return `
-  
-  `;
-  });
-  $app.innerHTML = output;
+const greetParcel = () => {
+  return `<h1>Hello parcel!</h1>`;
 };
 
-main();
+export function farewellParcel() {
+  return `<p>Goodbye parcel :(</p>`;
+}
+
+export const delayParcel = () => {
+  return new Promise((resolve, reject) => {
+    true
+      ? setTimeout(() => resolve(`<small>Parcel ( 2s later )</small>`), 2000)
+      : reject(`<small>Parcel ( error )</small>`);
+  });
+};
+
+export async function returnDelayParcel() {
+  const response = await delayParcel();
+  $app.innerHTML += response;
+}
+
+// export function counter() {
+//   const $strong = document.querySelector("strong");
+//   const $addButton = document.getElementById("add");
+//   const $subButton = document.getElementById("sub");
+//   let counter = 0;
+//   $strong.innerHTML = counter;
+//   $addButton.addEventListener("click", () => {
+//     counter += 1;
+//     $strong.innerHTML = counter;
+//   });
+//   $subButton.addEventListener("click", () => {
+//     counter -= 1;
+//     $strong.innerHTML = counter;
+//   });
+// }
+
+export default greetParcel;
+
+export const characterData = {
+  id: 1,
+  name: "Arthur Morgan",
+  image: ".png",
+  gender: "Masculine",
+  age: 36,
+  gang: "Van der Linde",
+  is_alive: false,
+  money: 13_574,
+  friends: [
+    "John Marston",
+    "Charles Smith",
+    "Sadie Adler",
+    "Lenny Summers",
+    "Hosea Matthews",
+  ],
+  graveLocation: "Mount Shann",
+  weight: "Underweight",
+  horses: ["Andalusian", "Turkoman", "Kentucky Saddler"],
+  atributtes: {
+    health: 100,
+    stamina: 100,
+    deadeye: 100,
+  },
+};
+
+export function vowelsCounter(text) {
+  const cleanText = text.toLowerCase().trim();
+  const vowels = {
+    a: 0,
+    e: 0,
+    i: 0,
+    o: 0,
+    u: 0,
+  };
+
+  for (let vowel of cleanText) {
+    if (
+      vowel === "a" ||
+      vowel === "e" ||
+      vowel === "i" ||
+      vowel === "o" ||
+      vowel === "u"
+    ) {
+      vowels[vowel] += 1;
+    }
+  }
+
+  return vowels;
+}
