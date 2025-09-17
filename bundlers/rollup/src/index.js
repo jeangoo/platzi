4f4b648d5d8d6e9d0b3bcb0be4056d0307daea61
Jean <jeaancast03@gmail.com>
2025-09-17 16:56:35 -0400

    changes

--- bundlers/rollup/src/index.js
+++ bundlers/rollup/src/index.js
@@ -6,16 +6,18 @@ const main = async () => {
   const response = await fetch(API_URL);
   const data = await response.json();
 
-  const output = data.users.map((user) => {
-    const { firstName, lastName, maidenName, email, image } = user;
-    return `
+  const output = data.users
+    .map((user) => {
+      const { firstName, lastName, maidenName, email, image } = user;
+      return `
   <article class="user-card">
   <img src=${image} alt=${firstName} ${lastName}>
   <span>${firstName} ${lastName} ${maidenName}</span>
   <p>${email}</p> 
   </article> 
   `;
-  }).join("");
+    })
+    .join("");
   const newItem = document.createElement("section");
   newItem.classList.add("user-cards-section");
   newItem.innerHTML = output;
