b96136baf295f1e40701063fd464a8b0b9df1692
Jean <jeaancast03@gmail.com>
2025-09-24 22:48:59 -0400

    new folder/files/changes

--- topack/src/index.js
+++ topack/src/index.js
@@ -1,7 +1,7 @@
-const $image = document.querySelector('img')
-const $quotes = document.getElementById('quotes')
+const $image = document.querySelector("img");
+const $quotes = document.getElementById("quotes");
 
-$image.src = '../assets/photo.avif'
+$image.src = "../assets/photo.avif";
 
 function fetchQuote() {
   fetch("https://api.quotable.io/random?tags=&author=")
--- topack/webpack.config.js
+++ topack/webpack.config.js
@@ -1,12 +1,76 @@
-const path = require('path')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contentHash].js",
+    assetModuleFilename: "assets/[hash][ext][query]"
   },
   resolve: {
-    extensions: ['.js']
+    extensions: [".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.avif/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.ttf$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: "application/font-ttf",
+            name: "[name].[contentHash].[ext]",
+            outputPath: "./fonts",
+            publicPath: "./fonts",
+            esModule: false,
+          }
+        }
+      }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].[contentHash].css'
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'assets'),
+          to: "assets"
+        }
+      ]
+    })
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ]
   }
-}
+};
